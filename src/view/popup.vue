0da93b6f9c41a2912561121228421bfcdd45b67c
unknown <toil.official@yandex.com>
2023-10-06 20:09:50 +0300

    added video summarize

--- src/view/popup.vue
+++ src/view/popup.vue
@@ -13,7 +13,20 @@
           {{ sharingData.title }}
         </p>
         <ul class="content-list" v-for="partData of sharingData.desc" :key="partData.id">
-          <li> {{ partData.content }}</li>
+          <template v-if="partData.theses">
+            <li>
+              <h3 @click="moveToTime(partData.start_time)">
+                <span class="content-time"> {{ formatSecs(partData.start_time) }} </span>
+                {{ partData.content }}
+              </h3>
+              <ul class="content-list" v-for="thesisData of partData.theses" :key="thesisData.id">
+                <li>{{ thesisData.content }}</li>
+              </ul>
+            </li>
+          </template>
+          <template v-else>
+            <li> {{ partData.content }}</li>
+          </template>
         </ul>
       </div>
       <div class="content-copy" v-if="sharingData?.sharingUrl">
@@ -52,9 +65,10 @@ export default {
   },
   created: async function () {
     const currentURL = await this.getTab();
+    const urlType = this.getURLType(currentURL);
     console.log(currentURL)
 
-    let data = await apiWrapper.getSharingUrl(currentURL);
+    let data = await apiWrapper.getSharingUrl(currentURL, urlType);
     console.log(data)
     if (!(data !== undefined && Object.keys(data).length)) {
       data = {
@@ -78,10 +92,36 @@ export default {
     async getTab() {
       let queryOptions = { active: true, currentWindow: true };
       let tabs = await chrome.tabs.query(queryOptions);
-      return tabs[0].url;
-    }
-  }
+      const url = tabs[0].url;
+      return url;
+    },
+    getURLType(url) {
+      const hostname = new URL(url).hostname;
+      const youtubeTest = /^(www.|m.)?youtube(-nocookie)?.com$/
+      return youtubeTest.test(hostname) ? 'video' : 'article';
+    },
+    formatSecs(secs) {
+      const hours = Math.floor(secs / 3600);
+      secs = secs - hours * 3600;
+      const mins = Math.floor(secs / 60);
+      secs = secs - mins * 60;
+      return `${hours ? this.formatDate(hours)+':' : '' }${this.formatDate(mins)}:${this.formatDate(secs)}`;
+    },
+    formatDate(dateString) {
+      // format X hours, minutes and etc to 0X. Ex. 1 -> 01, 12 -> 12
+      return ('0' + dateString).slice(-2);
+    },
+    // moveToTime(secs) {
+    //   const video = document.querySelector('video');
+    //   console.log(video)
+    //   if (!video) {
+    //     return false;
+    //   }
 
+    //   video.currentTime = secs;
+    //   return true;
+    // }
+  }
 }
 
 </script>
@@ -183,4 +223,9 @@ export default {
     justify-content: center;
     margin: 40px 0;
   }
+
+  .content-time {
+    color: #FF5317;
+  }
+
 </style>
